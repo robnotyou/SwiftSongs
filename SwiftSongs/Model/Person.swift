ff2c9da6f5c09195abb3f87a640d420141a5e7a8
Rob <rob@notyou.co.uk>
2020-03-28 09:25:14 +0000

    Additions

--- SwiftSongs/Model/Person.swift
+++ SwiftSongs/Model/Person.swift
@@ -10,23 +10,63 @@ import Foundation
 
 // MARK: - Person
 class Person {
+    var copula: Copula?
     
-    func love(_ lovee: Person) {
-        
+    func said(_ words: String)  {
     }
 }
 
 // MARK: - FirstPerson
 class FirstPerson: Person {
-    
+    func love(_ lovee: Person) {
+    }
 }
 
 // MARK: - SecondPerson
 class SecondPerson: Person {
     
+    func s(_ attribute: Attribute) {
+    }
+    func aint(_ attribute: Attribute) {
+    }
+    
+    func love(_ lovee: Person) {
+    }
 }
 
 // MARK: - ThirdPerson
 class ThirdPerson: Person {
     
+    func s(_ attribute: Attribute) {
+    }
+    func aint(_ attribute: Attribute) {
+    }
+    
+    func loves(_ lovee: Person) -> Person {
+        return lovee
+    }
+}
+
+
+
+// MARK: - Polarity
+enum Polarity {
+    case affirmation(aff: String)
+    case negation(neg: String)
+}
+
+
+
+// MARK: - Copula (no, really!)
+enum Copula {
+    case iS(_ attribute: Attribute)
+    case isNot(_ attribute: Attribute)
+}
+
+
+
+// MARK: - Attribute
+enum Attribute {
+    case heavy
+    case woman
 }
--- SwiftSongs/Model/Songs.swift
+++ SwiftSongs/Model/Songs.swift
@@ -8,6 +8,27 @@
 
 import Foundation
 
+// MARK: - Person
 class Songs {
     
+    let i = FirstPerson()
+    lazy var me = i
+    let you = SecondPerson()
+    let her = ThirdPerson()
+    let she = ThirdPerson()
+    let he = ThirdPerson()
+}
+
+
+
+// MARK: - Beatles
+extension Songs {
+    
+    func Beatles() {
+        let yeahYeahYeah = Polarity.affirmation(aff: "Yeah, Yeah, Yeah")
+        she.loves(you)
+        
+        she.s(.woman)
+        he.aint(.heavy)
+    }
 }
