2949bbcb45cf286f46bcec14101c86a69754fbf1
Rob <rob@notyou.co.uk>
2020-03-28 08:40:01 +0000

    Initial Commit

---
+++ SwiftSongs/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftSongs
+//
+//  Created by Rob on 28/03/2020.
+//  Copyright © 2020 Rob Dixon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
